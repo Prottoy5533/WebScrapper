6db63198cfc0cf63cab7732f7358973f12164b13
Abid <abid5533@gmail.com>
2025-01-25 20:38:31 +0600

    updated

---
+++ WebScrapper/DailyStarDetailsScrapper.cs
@@ -0,0 +1,153 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapper
+{
+    public class DailyStarDetailsScrapper
+    {
+        private readonly HttpClient _client;
+        private readonly INewsContentService _newsContentService;
+
+        public DailyStarDetailsScrapper(INewsContentService newsContentService)
+        {
+            _client = new HttpClient();
+
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+            _newsContentService = newsContentService;
+        }
+
+        public string GetHtmlContent(string url)
+        {
+            HttpResponseMessage response = _client.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public List<string> ExtractCategoryLink(string htmlContent)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(htmlContent);
+
+            
+            //var categoryNodes = document.DocumentNode.SelectNodes("//div[@class='columns main-menu-wrapper']//ul//li//a[@href]");
+            var categoryNodes = document.DocumentNode.SelectNodes("//div[@class='columns main-menu-wrapper']//ul//li//a[text()='Sports']");
+            //var categoryNodes = document.DocumentNode.SelectNodes("//nav//ul//li//a[text()='World']");
+            var categoryLinks = new HashSet<string>();
+
+
+            if (categoryNodes != null)
+            {
+                foreach (var node in categoryNodes)
+                {
+                    string href = node.GetAttributeValue("href", string.Empty);
+                    if (!string.IsNullOrEmpty(href) && !href.Contains("archive") && !href.Contains("search") && !href.Contains("/english") &&
+                !href.Equals("/news"))
+                    {
+
+                        categoryLinks.Add("https://www.thedailystar.net" + href);
+                    }
+                }
+            }
+
+            return categoryLinks.ToList();
+        }
+
+        public List<string> ExtractNewsLinks(string categoryUrl)
+        {
+            string htmlContent = GetHtmlContent(categoryUrl);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(htmlContent);
+
+            //var newsNodes = document.DocumentNode.SelectNodes("//div[@class='Cat-lead-wrapper']/a[@href] | //div[@class='Cat-list']/a[@href] | //div[@class='rm-container align-items-stretch']/a[@href] | //div[@class='tab-news']/a[@href]");
+            //var newsNodes = document.DocumentNode.SelectNodes("//div[@class='card-content pt-20 pb-20 pr-20']/h3[@class='title fs-20']/a[@href]");
+            //var newsNodes = document.DocumentNode.SelectNodes("//div[contains(@class, 'card-content pt-20 pb-20 pr-20')]//a[@href]");
+            var newsNodes = document.DocumentNode.SelectNodes("//div[contains(@class, 'card-content pt-20 pb-20 pr-20') or contains(@class, 'card-content card-content columns')]//a[@href]");
+
+            var newsLinks = new HashSet<string>();
+
+            if (newsNodes != null)
+            {
+                foreach (var node in newsNodes)
+                {
+                    string href = node.GetAttributeValue("href", string.Empty);
+                    if (!string.IsNullOrEmpty(href))
+                    {
+                        newsLinks.Add(href.StartsWith("http") ? href : "https://www.thedailystar.net" + href);
+                    }
+                }
+            }
+
+            return newsLinks.ToList();
+        }
+
+        public string ExtractFullNewsWithoutDate(string newsUrl, string categoryName)
+        {
+            string htmlContent = GetHtmlContent(newsUrl);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(htmlContent);
+
+            var titleNode = document.DocumentNode.SelectSingleNode("//div[@class='d-flex live']//h1");
+            var contentNodes = document.DocumentNode.SelectNodes("//p[not(@class='author') and not(@class='pub') and not(ancestor::div[@class='Brief']) and not(ancestor::div[@class='CopyRight']) and not(ancestor::div[@class='HeaderTopDate']) and not(ancestor::div[@class='pub-up print-section d-lg-flex']) and not(child::span[@class='author'])] | //span[not(@class='author') and not(@class='pub') and not(ancestor::div[@class='HeaderTopDate']) and not(ancestor::div[@class='pub-up print-section d-lg-flex'])]");
+
+            StringBuilder fullNews = new StringBuilder();
+
+            if (titleNode != null)
+            {
+                fullNews.AppendLine($"Title: {HtmlEntity.DeEntitize(titleNode.InnerText.Trim())}");
+                fullNews.AppendLine();
+            }
+            var content = "";
+            if (contentNodes != null)
+            {
+                foreach (var node in contentNodes)
+                {
+                    if (node.InnerText.Contains("+"))
+                    {
+                        continue;
+                    }
+
+                    string innerText = HtmlEntity.DeEntitize(node.InnerText.Trim());
+                    if (!string.IsNullOrEmpty(innerText) && !string.IsNullOrWhiteSpace(innerText))
+                    {
+                        fullNews.AppendLine(innerText);
+                        fullNews.AppendLine();
+                        content += innerText;
+                    }
+                }
+
+                var newsContent = new NewsContent();
+
+                if (titleNode != null)
+                {
+                    newsContent.Title = titleNode.InnerText.Trim();
+                    newsContent.Content = content;
+                    newsContent.Category = categoryName;
+                    newsContent.NewsPaperName = "BDNews24";
+                    newsContent.CreatedDate = DateTime.Now;
+
+                    _newsContentService.AddNewsContent(newsContent);
+                }
+
+
+            }
+
+            return fullNews.ToString();
+        }
+
+        public void WriteTextToFile(string text, string filePath)
+        {
+
+            string directoryPath = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(filePath, text);
+        }
+    }
+}
--- WebScrapper/Program.cs
+++ WebScrapper/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Hello, World!");
 
 
 
-string url = "https://bdnews24.com";
+
 //string url = "https://www.prothomalo.com/";
 string directoryPath = @"D:\WebScrapper\Output";
 //string filePath = Path.Combine(directoryPath, $"bdnews24_content_{DateTime.Now:yyyyMMdd_HHmmss}.txt");
@@ -21,12 +21,14 @@ var host = Host.CreateDefaultBuilder(args)
                // Register NewsContentService with DI as a singleton or scoped as needed
                services.AddScoped<INewsContentService, NewsContentService>();
                services.AddScoped<BdNewsDetailsScrapper>();
+               services.AddScoped<DailyStarDetailsScrapper>();
            })
            .Build();
 
 
 try
 {
+    string url = "https://bdnews24.com";
     var scraper = host.Services.GetRequiredService<BdNewsDetailsScrapper>();
     string mainPageHtml = scraper.GetHtmlContent(url);
 
@@ -41,7 +43,7 @@ try
         output = "";
         output += "Category: " + categoryName + Environment.NewLine + Environment.NewLine;
         List<string> newsLinks = scraper.ExtractNewsLinks(categoryLink);
-        
+
         foreach (var newsLink in newsLinks)
         {
             string fullNews = scraper.ExtractFullNewsWithoutDate(newsLink, categoryName);
@@ -55,10 +57,56 @@ try
             NewsPaperName = "BdNews24",
             CreatedDate = DateTime.Now
         };
-        
+
         newsContentService.AddNewsContentCategoryWise(newsContentCategory);
         string filePath = Path.Combine(directoryPath, $"bdnews24_content_{categoryName}_{DateTime.Now:yyyyMMdd_HHmm}.txt");
-        
+
+        scraper.WriteTextToFile(output, filePath);
+        Console.WriteLine($"Web scraping completed for {x[3]}. Content written to " + filePath);
+    }
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+
+try
+{
+    string url = "https://www.thedailystar.net/";
+    var scraper = host.Services.GetRequiredService<DailyStarDetailsScrapper>();
+    string mainPageHtml = scraper.GetHtmlContent(url);
+
+    List<string> categoryLinks = scraper.ExtractCategoryLink(mainPageHtml);
+
+    string output = "";
+    var newsContentService = host.Services.GetRequiredService<INewsContentService>();
+    foreach (var categoryLink in categoryLinks)
+    {
+        var x = categoryLink.Split("/");
+        var categoryName = x[3];
+        output = "";
+        output += "Category: " + categoryName + Environment.NewLine + Environment.NewLine;
+        List<string> newsLinks = scraper.ExtractNewsLinks(categoryLink);
+
+        foreach (var newsLink in newsLinks)
+        {
+            string fullNews = scraper.ExtractFullNewsWithoutDate(newsLink, categoryName);
+            output += fullNews + Environment.NewLine + Environment.NewLine;
+        }
+
+        var newsContentCategory = new NewsContentCategoryWise
+        {
+            Category = categoryName,
+            FullNews = output,
+            NewsPaperName = "BdNews24",
+            CreatedDate = DateTime.Now
+        };
+
+        newsContentService.AddNewsContentCategoryWise(newsContentCategory);
+        string filePath = Path.Combine(directoryPath, $"bdnews24_content_{categoryName}_{DateTime.Now:yyyyMMdd_HHmm}.txt");
+
         scraper.WriteTextToFile(output, filePath);
         Console.WriteLine($"Web scraping completed for {x[3]}. Content written to " + filePath);
     }
